9846a50722b7f43b00971f7c48d73ad2c7d767bf
Laisul Islam Tomal <90374087+Tomalislam@users.noreply.github.com>
2025-05-02 11:52:24 +0600

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,23 @@
+<script type="module">
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/11.6.1/firebase-app.js";
+  import { getAnalytics } from "https://www.gstatic.com/firebasejs/11.6.1/firebase-analytics.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  const firebaseConfig = {
+    apiKey: "AIzaSyD2oTeRHgWwA2RvBVQZWcBhi3UNw9l3w2Y",
+    authDomain: "e-printing-3ca34.firebaseapp.com",
+    projectId: "e-printing-3ca34",
+    storageBucket: "e-printing-3ca34.firebasestorage.app",
+    messagingSenderId: "51071340726",
+    appId: "1:51071340726:web:aa1a604db26a3c9d9a1617",
+    measurementId: "G-G0LSVYTBHJ"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
+</script>
